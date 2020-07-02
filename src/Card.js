ae14332a7af59f4758da28c79e2d61f57943a9ec
Ronnie Holguin <64113740+ronnieholguin@users.noreply.github.com>
2020-07-02 15:12:17 -0700

    init

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Card.css';
+
+
+export default function Card(props) {
+  return (
+    <div className='Card'>
+      <button
+        type='button'
+      >
+        delete
+      </button>
+    </div>
+  )
+}
+
+Card.propTypes = {
+  onClickDelete: () => {}
+}
